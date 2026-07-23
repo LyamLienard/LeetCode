66d2ca68d852f0e50fc5ee07ccbb8beb2537c415
LyamLienard <lyam.lienard@gmail.com>
2026-07-24 00:20:23 +0200

    Added SQL problem 176

---
+++ SQL/Medium/176 - Second Highest Salary.sql
@@ -0,0 +1,11 @@
+-- https://leetcode.com/problems/second-highest-salary/
+-- MySQL
+
+SELECT (
+    SELECT DISTINCT
+    salary 
+    FROM
+    Employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET 1
+) as SecondHighestSalary
