b38b56db8e0d59daf646852d08de86f7c24793e1
LyamLienard <lyam.lienard@gmail.com>
2026-07-28 17:07:35 +0200

    Added SQL problem 177

---
+++ SQL/Medium/177 - Nth Highest Salary.sql
@@ -0,0 +1,17 @@
+-- https://leetcode.com/problems/nth-highest-salary/description/
+-- MySQL
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+SET N = N - 1;
+  RETURN (
+      SELECT (
+        SELECT DISTINCT
+        salary
+        FROM
+        Employee
+        ORDER BY salary DESC
+        LIMIT 1 OFFSET N
+      )
+  );
+END
