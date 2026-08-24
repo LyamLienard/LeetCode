39c84ccfc377987330959ab1572e7f2f750849e9
LyamLienard <lyam.lienard@gmail.com>
2026-08-24 23:20:42 +0200

    Added SQL problem 577

---
+++ SQL/Easy/577 - Employee Bonus.sql
@@ -0,0 +1,10 @@
+-- https://leetcode.com/problems/employee-bonus/description/
+-- MySQL
+
+SELECT
+e.name,
+b.bonus
+FROM Employee AS e
+LEFT JOIN Bonus AS b
+ON e.empId = b.empId
+WHERE b.bonus < 1000 OR b.bonus IS NULL
