01db82d2ff6c8bd63a0e33a66def41c2ec8fd18a
LyamLienard <lyam.lienard@gmail.com>
2026-09-12 20:41:21 +0200

    Added Database Quest - P4 Q3

---
+++ SQL/Database Quests/Window Functions & Ranking Analysis Room/Q3 - Consecutive Numbers.sql
@@ -0,0 +1,9 @@
+-- https://leetcode.com/problems/consecutive-numbers/
+-- MySQL
+
+SELECT DISTINCT
+l1.num AS ConsecutiveNums
+FROM Logs AS l1
+INNER JOIN Logs AS l2 ON l1.id + 1 = l2.id
+INNER JOIN Logs AS l3 ON l2.id + 1 = l3.id
+WHERE l1.num = l2.num AND l2.num = l3.num
